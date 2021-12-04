e2410a095108238d70f28483e2e76280c164fa97
michaljarnot <m.jarnot@yahoo.com>
2021-12-04 20:00:20 +0100

    feat: example fixed ??

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,25 @@
 #include <napi.h>
+#include <iostream>
 
-class Vec : public Napi::ObjectWrap<Vec> {
+#include<stdio.h>
+#include<string.h>
+
+template<class Policy>
+class Vec : public Napi::ObjectWrap<Vec<Policy>> {
 public:
   static Napi::Object Init(Napi::Env env, Napi::Object exports) {
     Napi::HandleScope scope(env);
 
-    Napi::Function func = Napi::ObjectWrap<Vec>::DefineClass(env, "Vec", {});
+    Napi::Function func = Napi::ObjectWrap<Vec<Policy>>::DefineClass(env, Policy::name, {});
 
     constructor = Napi::Persistent(func);
     constructor.SuppressDestruct();
 
-    exports.Set("Vec", func);
+    exports.Set(Policy::name, func);
     return exports;
   }
 
-  explicit Vec(const Napi::CallbackInfo &info) : Napi::ObjectWrap<Vec>(info) {
+  explicit Vec(const Napi::CallbackInfo &info) : Napi::ObjectWrap<Vec<Policy>>(info) {
     Napi::Env env = info.Env();
     Napi::HandleScope scope(env);
   }
@@ -23,10 +28,17 @@ private:
   static Napi::FunctionReference constructor;
 };
 
-Napi::FunctionReference Vec::constructor;
+template<class Policy>
+Napi::FunctionReference Vec<Policy>::constructor;
+
+struct Vec2dPolicy {
+  constexpr static char *name = "Vec2";
+  constexpr static int  Count  = 2;
+  using ElementType = double;
+};
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  Vec::Init(env, exports);
+  Vec<Vec2dPolicy>::Init(env, exports);
 
   return exports;
 }
